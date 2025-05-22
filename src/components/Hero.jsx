508293b3937602622302a1c23e69e2f1601b5776
rajatsaini2003 <rajatsaini9460@gmail.com>
2025-05-23 01:19:11 +0530

    feat: add Button component and integrate GSAP animations in Hero component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,8 @@
 import React, { useRef, useState } from 'react'
-
+import Button from './Button';
+import { TiLocationArrow } from 'react-icons/ti';
+import { useGSAP } from '@gsap/react';
+import gsap from 'gsap';
 const Hero = () => {
     const [currentIndex, setCurrentIndex] = useState(1);
     const [hasClicked, setHasClicked] = useState(false);
@@ -16,6 +19,24 @@ const Hero = () => {
         setHasClicked(true)
         setCurrentIndex(upcomingVideoIndex)
     }
+    useGSAP(()=>{
+        if(hasClicked){
+            gsap.set('#next-video', {visibility:'visible'});
+
+            gsap.to('#next-video',{
+                transformOrigin:'center center',
+                scale:1,
+                width:'100%',
+                height:'100%',
+                duration: 1,
+                ease: 'power1.inOut',
+                onStart:()=> nextVideoRef.current.play(),
+            })
+            gsap.from('#current-video',{
+                transformOrigin:'center center'
+            })
+        }
+    }, {dependencies:[currentIndex],revertOnUpdate:true})
     const getVideoSrc = (index) => `videos/hero-${index}.mp4`;
   return (
     <div className='relative h-dvh w-screen overflow-x-hidden'>
@@ -61,8 +82,27 @@ const Hero = () => {
                 />
             </div>
 
+            <h1 className='special-font hero-heading absolute bottom-5
+            right-5 z-40 text-blue-75'> 
+                G<b>a</b>ming
+            </h1>
+            <div className='absolute left-0 top-0 z-40 size-full'>
+                <div className='mt-24 px-5 sm:px-10'>
+                    <h1 className='special-font hero-heading text-blue-100'>
+                        re<b>d</b>efi <b>n</b>e
+                    </h1>
+                    <p className='mb-5 max-w-64 font-robert-regular text-blue-100'> 
+                        Enter the MetaGame Layer <br/> Unleash the Play Economy
+                    </p>
+                    <Button id="watch-trailer" title="Watch Trailer "
+                    leftIcon={<TiLocationArrow/>}  containerClass="bg-yellow-300 flex-center gap-1"/>
+                </div>
+            </div>
         </div>
-      
+        <h1 className='special-font hero-heading absolute bottom-5
+        right-5  text-black'> 
+            G<b>a</b>ming
+        </h1>
     </div>
   )
 }
